dd05daeba915a524e2059b601e26ac3305ec5d7c
Emre Canitez <ismailemrecanitez@gmail.com>
2023-11-24 00:34:44 +0300

    create_network, initialize_parameters and add_layer functions are added.

--- NNLib.py
+++ NNLib.py
@@ -1,12 +1,21 @@
 import numpy as np
 import pandas as pd
 
-
+class Layer:
+    def __init__(self,dimension = 0,activation_function = "",is_input_layer = False, is_output_layer = False):
+        if dimension != 0:
+            self.dimension = dimension
+            self.activation_function = activation_function
+        else:
+            raise ValueError("Dimension must be different from zero!")
+    
 
 class NNLib:
     def __init__(self,learning_rate):
-        self.weights = np.array([np.random.randn(), np.random.randn()])
-        self.bias = np.random.randn()
+        self.weights = {}
+        self.bias = {}
+        self.model = {}
+        self.layers = []
         self.learning_rate = learning_rate
 
 
@@ -50,3 +59,25 @@ class NNLib:
     def derivative_relu(self,data):
         data = [1 if value>0 else 0 for value in data]
         return np.array(data, dtype=float)
+    
+    def create_network(self,train_data):
+        layer = Layer(train_data.shape[1],is_input_layer=True)
+        self.layers.append(layer)
+             
+    def initialize_parameters(self, method = ""):
+        if method == "He":
+            if len(self.layers) < 2:
+                raise ValueError("There is no added layer!")
+            else:
+                self.weights["0"] = np.random.randn(self.layers[1].dimension, self.layers[0].dimension) * np.sqrt(2/self.layers[0].dimension)
+                self.bias["0"] = np.zeros((self.layers[1].dimension, 1))
+                
+    def add_layer(self,dimension,activation_function):
+        layer = Layer(dimension,activation_function)
+        self.layers.append(layer)
+        
+    def train(self):
+        pass
+    
+    def predict(self):
+        pass
